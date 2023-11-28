ae21d98cccfc8d8a210faf6822bf29ffc5226b93
TheLittle_Yang <2570984321@qq.com>
2023-11-28 11:33:15 +0800

    增加弹射物伤害跟踪

--- src/main/java/io/github/codevine327/fightpapi/PlayerListener.java
+++ src/main/java/io/github/codevine327/fightpapi/PlayerListener.java
@@ -1,6 +1,7 @@
 package io.github.codevine327.fightpapi;
 
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -21,6 +22,12 @@ public class PlayerListener implements Listener {
     @EventHandler
     private void onPlayerAttackEntity(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player player) {
+            FightPAPI.getInstance().getPlayerData().get(player).addAttackData((int) event.getFinalDamage());
+            return;
+        }
+
+        if (event.getDamager() instanceof Projectile projectile &&
+                projectile.getShooter() instanceof Player player) {
             FightPAPI.getInstance().getPlayerData().get(player).addAttackData((int) event.getFinalDamage());
         }
     }
